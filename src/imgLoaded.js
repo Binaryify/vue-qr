c37440e0b6b39cd313535b51e5fef2773dfb0d15
binaryify <zhuangtongfa.com>
2018-09-05 16:05:40 +0800

    修复 crossOrigin 问题

--- src/imgLoaded.js
+++ src/imgLoaded.js
@@ -1,14 +1,21 @@
 function imgLoaded(url) {
   return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.crossOrigin = ''
-    img.onload = function() {
-      resolve(img)
+    const xhr = new XMLHttpRequest()
+    xhr.onload = function() {
+      const url = URL.createObjectURL(this.response)
+      const img = new Image()
+      img.onload = function() {
+        resolve(img)
+        URL.revokeObjectURL(url)
+      }
+      img.onerror = function() {
+        reject('Image load error')
+      }
+      img.src = url
     }
-    img.onerror = function(err) {
-      reject('image load error,reason:' + JSON.stringify(err))
-    }
-    img.src = url
+    xhr.open('GET', url, true)
+    xhr.responseType = 'blob'
+    xhr.send()
   })
 }
 export default imgLoaded
