bae2b79c4149bda2865b0b797cb1ee1dfda9f6d1
binaryify <zhuangtongfa.com>
2019-03-27 16:44:26 +0800

    support gif background, refactoring, update to 2.0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <vue-qr text="hello world" :correctLevel="0"></vue-qr>
-        <!-- <vue-qr :bgSrc="src" :logoSrc="src2" text="Hello world!" :size="260" :margin="0" :dotScale="0.5"></vue-qr>
+        <vue-qr :bgSrc="src" :logoSrc="src2" text="Hello world!" :size="260" :margin="0" :dotScale="0.5"></vue-qr>
         <vue-qr :text="time+''"></vue-qr>
         <vue-qr :bgSrc="src3" text="Hello world!" :size="260" :margin="0" :dotScale="0.6"></vue-qr>
         <vue-qr :bgSrc="src4" text="Hello world!" :size="300" :dotScale="0.5"></vue-qr>
@@ -9,7 +9,7 @@
         <vue-qr text="测试内容测试内容测试内容测试内容测试内容测试内容" :gifBgSrc="require('./assets/dog.gif')" :size="300" :dotScale="0.4"></vue-qr>
         <vue-qr text="测试内容测试内容测试内容测试内容测试内容测试内容" :gifBgSrc="require('./assets/scare.gif')" :size="300"></vue-qr>
         <vue-qr text="测试内容测试内容测试内容测试内容测试内容" :gifBgSrc="require('./assets/scare.gif')" :size="300" backgroundDimming="rgb(255,0,0)" colorDark="rgb(0,0,0)" :dotScale="0.5"></vue-qr>
-        <vue-qr text="test"></vue-qr> -->
+        <vue-qr text="test"></vue-qr>
     </div>
 </template>
 
